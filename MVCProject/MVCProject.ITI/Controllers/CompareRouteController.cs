e7ad03a80c6d420082d7043b1553ae09b163c7c9
Shahd Mohy <shahdmohy00@gmail.com>
2026-05-07 01:47:45 +0300

    Authorization

--- MVCProject/MVCProject.ITI/Controllers/CompareRouteController.cs
+++ MVCProject/MVCProject.ITI/Controllers/CompareRouteController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MVCProject.ITI.Controllers
 {
+    [Authorize]
     public class CompareRouteController : Controller
     {
 
