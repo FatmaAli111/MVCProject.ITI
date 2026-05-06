ae2e59096643cf4ef7c15c94b23b67b2ff1c8003
Shahd Mohy <shahdmohy00@gmail.com>
2026-05-07 01:46:23 +0300

    Landing pages redirect authenticated users

--- MVCProject/MVCProject.ITI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MVCProject/MVCProject.ITI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -81,14 +81,24 @@ namespace MVCProject.ITI.Areas.Identity.Pages.Account
             public string ConfirmPassword { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
         {
+            
+            if (_signInManager.IsSignedIn(User))
+                return RedirectToAction("Dashboard", "Dashboard");
+
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
+            
+            if (_signInManager.IsSignedIn(User))
+                return RedirectToAction("Dashboard", "Dashboard");
+
             returnUrl ??= Url.Content("~/Dashboard/Dashboard");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
